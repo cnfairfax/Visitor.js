665d6c58beffcb8a571d5ffef7e7791cd33ed1a0
Chris Fairfax <cfairfax2010@gmail.com>
2017-09-16 21:25:45 -0400

    Added Visitor.js file as is. In the process of providing fail-safes for when jQuery is not available.

---
+++ Visitor.js
@@ -0,0 +1,108 @@
+var Visitor = {
+    data : {
+        contact : {},
+        lead : {}
+    },
+    write : function() {
+        Visitor.fetch();
+        var inputs = jQuery('.dynamo-form input');
+
+        Visitor.update(inputs);
+
+        var cookieString = JSON.stringify(Visitor.data);
+        // Set expiration date
+        var d = new Date();
+        d.setTime(d.getTime() + (7776000000));
+        var expires = "expires=" + d.toUTCString();
+        // write the cookie
+        document.cookie = 'Visitor=' + cookieString + ';' + expires + ';path=/';
+    },
+    render : function() {
+        Visitor.fetch();
+        var valueFields = jQuery('dyco');
+        valueFields.each(function(i){
+            var dynamo = jQuery(this).attr('data-dy-name');
+            if(Visitor.data[dynamo]) {
+                jQuery(this).text(Visitor.data[dynamo].value);
+            }
+        });
+    },
+    fetch : function() {
+        vString = Visitor.getVCookie('Visitor');
+
+        if (vString != '') {
+            Visitor.data = JSON.parse(vString);
+        }
+    },
+    update : function(inputs) {
+        for(i = 0; i < inputs.length; i++){
+            // Check to see if the input name exists in the array ecludedInputs
+            // indexOf() returns -1 when the value has no index in the array
+            // Only write values if they are NOT in the array excludedInputs
+            var exclude = inputs[i].attributes['data-dy-exclude'] ? inputs[i].attributes['data-dy-exclude'].value.toUpperCase() : 'NO';
+            var excluded = false;
+            if(exclude.indexOf('Y') == 0){
+                excluded = true;
+            }
+            if(!excluded) {
+                    // Check to see if an input has at least a leadfield or contactfield attribute
+                if(inputs[i].attributes['data-dy-name']){
+                    var dycoName = inputs[i].attributes['data-dy-name'].value;
+                    Visitor.data[dycoName] = new vAttribute(inputs[i]);
+                } else {
+                    Visitor.data[inputs[i].name] = new vAttribute(inputs[i]);
+                }
+            }
+        }
+    },
+    getVCookie : function(cookname) {
+        var name = cookname + "=";
+        // pull all cookies in a decoded format
+        var decodedCookie = decodeURIComponent(document.cookie);
+        // split cookies into individual cookies
+        var ca = decodedCookie.split(';');
+        var cString = '';
+        // cycle through cookies looking for cookie that begins w/ name
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            // trim leading spaces
+            c = c.trim();
+            // if cookie found, set cString to cookie value
+            if (c.indexOf(name) == 0) {
+                cString = c.substring(name.length, c.length);
+            }
+        }
+        return cString;
+    }
+}
+
+function vAttribute(input) {
+    if(input.attributes['data-dy-name']) {
+        this.name = inputs[i].attributes['data-dy-name'];
+    } else {
+        this.name = input.name;
+    }
+    this.value = input.value;
+}
+
+window.onload = function() {
+    if(typeof jQuery == 'function' || typeof $ == 'function') {
+        if(typeof jQuery != 'function') {
+            var jQuery = $;
+        }
+        jQuery(document).ready(function(){
+            Visitor.render(jQuery);
+            jQuery('.dynamo-form form').submit(function(){
+                Visitor.write();
+            });
+        });
+    } else {
+        document.addEventListener('DOMContentLoaded',function(){
+            Visitor.render();
+            var form = document.getElementsByClassName('dynamo-form');
+            form[0].getElementsByTagName('form')[0].addEventListener('submit', function(){
+                Visitor.write();
+            });
+        });
+    }
+}
