4df0c60e61a5ecc20bfe12e49711e0cdb9ad21f9
Christopher Fairfax <cfairfax2010@gmail.com>
2018-02-19 22:03:30 -0500

    Complete refactor, catching repo up...

--- Visitor.js
+++ Visitor.js
@@ -1,13 +1,19 @@
-var Visitor = {
-    data : {
+function Visitor() {
+    // capture 'this' for private methods
+    var Visitor = this;
+    // establish object member for visitor data
+    this.data = {
         contact : {},
         lead : {}
-    },
-    write : function() {
-        Visitor.fetch();
-        var inputs = jQuery('.dynamo-form input');
+    }
 
-        Visitor.update(inputs);
+    // update the cookie and write it back to memory
+    this.write = function() {
+        // update the Visitor.data object and then write updated information to a cookie
+        fetch();
+        var inputs = clickd_jquery('.clickdform input, .clickdform select');
+
+        update(inputs);
 
         var cookieString = JSON.stringify(Visitor.data);
         // Set expiration date
@@ -15,47 +21,142 @@ var Visitor = {
         d.setTime(d.getTime() + (7776000000));
         var expires = "expires=" + d.toUTCString();
         // write the cookie
-        document.cookie = 'Visitor=' + cookieString + ';' + expires + ';path=/';
-    },
-    render : function() {
-        Visitor.fetch();
-        var valueFields = jQuery('dyco');
-        valueFields.each(function(i){
-            var dynamo = jQuery(this).attr('data-dy-name');
-            if(Visitor.data[dynamo]) {
-                jQuery(this).text(Visitor.data[dynamo].value);
-            }
-        });
-    },
-    fetch : function() {
-        vString = Visitor.getVCookie('Visitor');
+        document.cookie = 'clickdynamo=' + cookieString + ';' + expires + ';path=/';
+    }
 
+    // render all called values to the page in this order:
+    // display if blocks
+    // render visitor data to cd elements
+    // set input values to visitor data
+    this.render = function() {
+        fetch();
+        switchIfs();
+        renderAttrs();
+        updateInputVals();
+    }
+
+    // fetch the cookie and update Visitor.data
+    function fetch() {
+        vString = getVCookie('clickdynamo');
+        // check to make sure the returned cookie is not empty
         if (vString != '') {
             Visitor.data = JSON.parse(vString);
-        }
-    },
-    update : function(inputs) {
-        for(i = 0; i < inputs.length; i++){
-            // Check to see if the input name exists in the array ecludedInputs
-            // indexOf() returns -1 when the value has no index in the array
-            // Only write values if they are NOT in the array excludedInputs
-            var exclude = inputs[i].attributes['data-dy-exclude'] ? inputs[i].attributes['data-dy-exclude'].value.toUpperCase() : 'NO';
-            var excluded = false;
-            if(exclude.indexOf('Y') == 0){
-                excluded = true;
-            }
-            if(!excluded) {
-                    // Check to see if an input has at least a leadfield or contactfield attribute
-                if(inputs[i].attributes['data-dy-name']){
-                    var dycoName = inputs[i].attributes['data-dy-name'].value;
-                    Visitor.data[dycoName] = new vAttribute(inputs[i]);
-                } else {
-                    Visitor.data[inputs[i].name] = new vAttribute(inputs[i]);
+            // cycle through the Visitor.data object,
+            // re-instantiating each property as a vAttribute object
+            for(var key in Visitor.data) {
+                if(Visitor.data.hasOwnProperty(key)) {
+                    for(var child in Visitor.data[key]) {
+                        if(Visitor.data[key].hasOwnProperty(child) && Visitor.data[key][child].hasOwnProperty('name')) {
+                            Visitor.data[key][child]  = new vAttribute(Visitor.data[key][child], Visitor.data[key][child].name);
+                        }
+                    }
+                    if(Visitor.data[key].hasOwnProperty('name')) {
+                        Visitor.data[key]  = new vAttribute(Visitor.data[key], Visitor.data[key].name);
+                    }
                 }
             }
         }
-    },
-    getVCookie : function(cookname) {
+    }
+
+    // capture form data and update the Visitor.data object
+    function update(inputs) {
+        var excludedInputs = ['cd_postsettings', 'cd_domainalias', 'cd_timezone', 'cd_domain', 'cd_accountkey', 'reqField', '', 'iQapTcha'];
+        for(i = 0; i < inputs.length; i++){
+            // Check to see if the input name exists in the array ecludedInputs
+            // Only write values if they are NOT in the array excludedInputs
+            if(excludedInputs.indexOf(inputs[i].name) == -1 && inputs[i].value != ''){
+                // Check to see if an input has at least a leadfield or contactfield attribute
+                // if not, assign an empty string to the variable(s)
+                var leadField = inputs[i].attributes['leadfield'] ? inputs[i].attributes['leadfield'].value : '';
+                var contactField = inputs[i].attributes['contactfield'] ? inputs[i].attributes['contactfield'].value : '';
+                // does leadField/contactField contain a value?
+                // if so, assign the attribute name using that value
+                // otherwise, assign it using the input name
+                if(leadField != '' || contactField != ''){
+                    // check to see if input's leadfield and contactfield values are the same
+                    // if so, write to the Visitor
+                    // otherwise, write to Visitor.lead and/or Visitor.contact separately
+                    if(leadField == contactField) {
+                        Visitor.data[leadField] = new vAttribute(inputs[i], leadField);
+                    } else { 
+                        if(leadField != '') {
+                            Visitor.data.lead[leadField] = new vAttribute(inputs[i], leadField);
+                        }
+                        if(contactField != '') {
+                            Visitor.data.contact[contactField] = new vAttribute(inputs[i], contactField);
+                        }
+                    }
+                } else {
+                    Visitor.data[inputs[i].name] = new vAttribute(inputs[i], inputs[i].name);
+                }
+            }
+        }
+    }
+
+    // determine which if statements should be displayed and unhide
+    function switchIfs() {
+        var ifElements = clickd_jquery('cd-if');
+        ifElements.each(function(i){
+            var dynamoIf = clickd_jquery(this).attr('data-dy-if');
+            if(dynamoIf.indexOf('=') != -1) {
+                dynamoIf = dynamoIf.split('=');
+                if(dynamoIf[0] != '' && dynamoIf[0].indexOf('.') != -1) {
+                    dynamoIf[0] = dynamoIf[0].split('.');
+                    if(Visitor.data[dynamoIf[0][0]][dynamoIf[0][1]].type == 'select-one' && Visitor.data[dynamoIf[0][0]][dynamoIf[0][1]].textValue == dynamoIf[1]) {
+                        this.style.display = '';
+                    } else if(Visitor.data[dynamoIf[0][0]][dynamoIf[0][1]].value == dynamoIf[1]) {
+                        this.style.display = '';
+                    }
+                } else if(dynamoIf[0] != '') {
+                    if(Visitor.data[dynamoIf[0]].type == 'select-one' && Visitor.data[dynamoIf[0]].textValue == dynamoIf[1]) {
+                        this.style.display = '';
+                    } else if(Visitor.data[dynamoIf[0]].value == dynamoIf[1]) {
+                        this.style.display = '';
+                    }
+                }
+            }
+        });
+    }
+
+    // render visitor information to cd elements
+    function renderAttrs() {
+        var valueFields = clickd_jquery('cd');
+        valueFields.each(function(i){
+            var dynamo = clickd_jquery(this).attr('data-dynamo');
+            if(dynamo.indexOf('.') != -1){
+                dynamo = dynamo.split('.');
+                if(Visitor.data[dynamo[0]][dynamo[1]]) {
+                    Visitor.data[dynamo[0]][dynamo[1]].render(this);
+                }
+            } else if(Visitor.data[dynamo]) {
+                Visitor.data[dynamo].render(this);
+            }
+        });
+    }
+
+    // set form input values to visitor data
+    function updateInputVals() {
+        var cdInputs = clickd_jquery('.clickdform input, .clickdform select');
+        cdInputs.each(function(i){
+            var leadField = clickd_jquery(this).attr('leadfield') ? clickd_jquery(this).attr('leadfield') : '';
+            var contactField = clickd_jquery(this).attr('contactfield') ? clickd_jquery(this).attr('contactfield') : '';
+            var inputName = this.name; 
+            if(leadField != '' || contactField != ''){
+                if(leadField == contactField && Visitor.data[leadField] && Visitor.data[leadField].name == leadField) {
+                    clickd_jquery(this).val(Visitor.data[leadField].value);
+                } else if(Visitor.data.lead[leadField] && Visitor.data.lead[leadField].name == leadField) {
+                    clickd_jquery(this).val(Visitor.data.lead[leadField].value);
+                } else if(Visitor.data.contact[contactField] && Visitor.data.contact[contactField].name == contactField) {
+                    clickd_jquery(this).val(Visitor.data.contact[contactField].value);
+                }
+            } else if(Visitor.data[inputName] && Visitor.data[inputName].name == inputName) {
+                clickd_jquery(this).val(Visitor.data[inputName].value);
+            }
+        });
+    }
+
+    // call cookie from memory - returns the string value of the cookie
+    function getVCookie(cookname) {
         var name = cookname + "=";
         // pull all cookies in a decoded format
         var decodedCookie = decodeURIComponent(document.cookie);
@@ -76,33 +177,57 @@ var Visitor = {
     }
 }
 
-function vAttribute(input) {
-    if(input.attributes['data-dy-name']) {
-        this.name = inputs[i].attributes['data-dy-name'];
+// Attribute constructor
+function vAttribute(input, iName) {
+    // check for 'select' inputs and add a human-readable "textValue" to allow for meaningful rendered data
+    if(input.type == 'select-one') {
+        if(input.innerHTML){
+            var optionText = clickd_jquery('option[value="' + clickd_jquery(input).val() + '"]', clickd_jquery(input)).text().trim();
+            if(optionText != ''){
+                this.textValue = optionText;
+                this.name = iName;
+                this.value = input.value;
+                this.type = input.type;
+            }
+        } else {
+            this.textValue = input.textValue;
+            this.name = iName;
+            this.value = input.value;
+            this.type = input.type;
+        }
     } else {
-        this.name = input.name;
+        this.name = iName;
+        this.value = input.value;
+        this.type = input.type;
     }
-    if(input.type = 'select-one') {
-        this.valueLabel = input.innerHTML ? input.querySelector('option[value="' + input.value + '"]').textContent : input.valueLabel;
-    }
-    this.type = input.type;
-    this.value = input.value;
 }
 
+// add the render method to the vAttribute prototype so an individual render method isn't created
+// for each individual attribute
 vAttribute.prototype.render = function(element) {
     if(this.type == 'select-one') {
-        element.textContent = this.valueLabel;
+        clickd_jquery(element).text(this.textValue);
     } else {
-        element.textContent = this.value;
+        clickd_jquery(element).text(this.value);
     }
 }
 
-window.onload = function() {
-    document.addEventListener('DOMContentLoaded',function(){
-        Visitor.render();
-        var form = document.querySelector('.dynamo-form form');
-        form.addEventListener('submit', function(){
-            Visitor.write();
-        });
-    });
+// this is necesarry for setup during testing, but should be replaced by default CSS
+var hiddenIfs = document.querySelectorAll('cd-if');
+hiddenIfs.forEach(function(element){
+    element.style.display = 'none';
+});
+
+// generate Visitor
+if(Object.prototype.toString.call(clickd_jquery) != '[object Function]' && Object.prototype.toString.call(jQuery) == '[object Function]') {
+    var clickd_jquery = jQuery.noConflict(true);
+} else if(Object.prototype.toString.call(clickd_jquery) != '[object Function]' && Object.prototype.toString.call($) == '[object Function]') {
+    var clickd_jquery = $.noConflict(true);
 }
+clickd_jquery(document).ready(function(){
+    var v = new Visitor();
+    v.render();
+    clickd_jquery('.clickdform').submit(function(){
+        v.write();
+    });
+});
